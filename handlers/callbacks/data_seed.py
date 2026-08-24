ca934ef32b37830449365cdcf78d7a59b7693c6a
mi <dontlistenthem@protonmail.com>
2026-08-24 15:52:02 +0300

    Update data_seed.py

--- handlers/callbacks/data_seed.py
+++ handlers/callbacks/data_seed.py
@@ -3,77 +3,71 @@ from aiogram.enums import ParseMode
 from aiogram.types import CallbackQuery
 from aiogram.fsm.context import FSMContext
 
-from storage.db import sql3_receipt_row
+from aiomysql import Pool
+
+
+from storage.db import init_receipt_row
 from keyboards.keyboard import (
     blockchain_keyboard,
     data_action_keyboard,
-    action_keyboard,
     back_menu_keyboard
 )
 
 seed_router = Router()
 
-@seed_router.callback_query(lambda trk: trk.data == 'track')
-async def track_back(callback: CallbackQuery, state: FSMContext) -> None: 
-    await callback.answer()
-    step_data = await state.get_data()
-    last_step = step_data.get('action')
+@seed_router.callback_query(lambda callback: callback.data in {'track', 'my_wallets'})
+async def track_back(
+    pl: Pool,
+    callback: CallbackQuery, 
+    state: FSMContext
+    ) -> None: 
     
     client_id = callback.from_user.id
-    data = await sql3_receipt_row(
-        client=client_id
-    )
-    if data != None:
-        if len(data) >= 5:
-            await callback.bot.edit_message_text(chat_id=client_id,
-                                                 message_id=last_step,
-                                                 text=f'📚 You have too many addresses!',
-                                                 reply_markup=back_menu_keyboard)
-            return
-    await callback.bot.edit_message_text(chat_id=client_id,
-                                        message_id=last_step,
-                                        text=f'<b>Select blockchain</b>',
-                                        reply_markup=blockchain_keyboard,
-                                        parse_mode=ParseMode.HTML)
-            
-@seed_router.callback_query(lambda mywall: mywall.data == 'my_wallets')
-async def my_wallets_back(callback: CallbackQuery, state: FSMContext) -> None:
-    step_data = await state.get_data()
-    last_step = step_data.get('action')
-    
-    client_id = callback.from_user.id
-    
-    data = await sql3_receipt_row(
-        client=client_id
+    if callback.data == 'track':
+        last_stage = await state.get_data()
+        about_stage = last_stage.get('action')
+        async with pl.acquire() as cnn:
+            async with cnn.cursor() as cr:
+                raw_rows = await cr.execute('SELECT rows FROM Table_1 WHERE client_id = %s', (client_id, ))
+                if raw_rows != None:
+                    rows = await cr.fetchall()
+        if len(rows) >= 5:
+            text = f'📚 You have too many addresses!'
+            markup = back_menu_keyboard
+        else:
+            text = f'<b>Select blockchain</b>'
+            markup = blockchain_keyboard
+        await callback.bot.edit_message_text(
+            text=text,
+            chat_id=client_id,
+            message_id=about_stage,
+            reply_markup=markup,
+            parse_mode=ParseMode.HTML
         )
-    if data != None:
-        rows = '\n\n'.join(
+    elif callback.data == 'my_wallets':
+        data = await init_receipt_row(
+            client=client_id
+            )
+        if data != None:
+            rows = '\n\n'.join(
                 f'{i}. {row}'
-                for i, row in enumerate(data,
-                                        start=1)
-        )
-        await callback.bot.edit_message_text(chat_id=client_id,
-                                             message_id=last_step,
-                                             text=f'<b>📋 Your addresses at moment:</b>\n\n\n'
-                                             f'<code>{rows}</code>',
-                                             reply_markup=data_action_keyboard,
-                                             parse_mode=ParseMode.HTML)
-        return
-    await callback.bot.edit_message_text(chat_id=client_id,
-                                         message_id=last_step,
-                                         text=f'🤷‍♀️ You not have addresses it yet, added their!',
-                                         reply_markup=back_menu_keyboard)
-    await state.clear()
-    return
-    
-@seed_router.callback_query(lambda back: back.data == 'back_menu')
-async def back_to_menu(callback: CallbackQuery, state: FSMContext) -> None:
-    await state.clear()
-
-    main_menu = await callback.message.edit_text(text=f'↩️ Your return to main menu!\n\n\n'
-                                                      f'<b>👇 Select action by clicking the button below</b>',
-                                                 reply_markup=action_keyboard,
-                                                 parse_mode=ParseMode.HTML)
-    await state.update_data(
-        action=main_menu.message_id
+                for i, row in enumerate(
+                    data,
+                    start=1)
+            )
+            last_stage = await state.get_data()
+            about_stage = last_stage.get('action')
+            text = (f'<b>📋 Your addresses at moment:</b>\n\n\n'
+                    f'<code>{rows}</code>')
+            markup = data_action_keyboard
+        else:
+            text = f'🤷‍♀️ You not have addresses it yet, added them!'
+            markup = back_menu_keyboard
+            await state.clear()
+        await callback.bot.edit_message_text(
+            text=text,
+            chat_id=client_id,
+            message_id=about_stage,
+            reply_markup=markup,
+            parse_mode=ParseMode.HTML
         )
